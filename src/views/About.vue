96f41ef4fb79006240cc4afbf0229ea666c53de8
Bestian Tang <bestian@gmail.com>
2020-05-07 10:43:23 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,25 @@
 <template>
   <div class="about">
     <v-container>
-      <h1>This is an about page</h1>
+        <v-row class="text-center">
+          <v-col class="mb-4">
+            <h1>關於本站</h1>
+            <h3>相關連結:</h3>
+            <v-list>
+              <v-list-item link href="https://www.gymirin.com/findagym" target="_blank">
+                *GYMIRIN自由教練找場地
+              </v-list-item>
+            </v-list>
+          </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
+
+<style type="text/css" scoped="">
+  .v-list-item {
+    justify-content: center;
+    text-align: left;
+    display: grid;
+  }
+</style>
