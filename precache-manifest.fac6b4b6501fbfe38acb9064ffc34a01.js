c967b531fe29d57957e2bd245cd261f1edcc1950
Bestian Tang <bestian@gmail.com>
2020-05-11 14:22:13 +0800

    deploy

---
+++ precache-manifest.fac6b4b6501fbfe38acb9064ffc34a01.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa5fc0cb4a4d577f8da25c95aa81548",
+    "url": "/coach/coaches.json"
+  },
+  {
+    "revision": "6cbc9e8d8c52b0e480a4",
+    "url": "/coach/css/app.733857a2.css"
+  },
+  {
+    "revision": "ea77fc657fcb648f174b",
+    "url": "/coach/css/chunk-a4a4dcf4.688d0308.css"
+  },
+  {
+    "revision": "5df2eb6db305732b1280",
+    "url": "/coach/css/chunk-vendors.e9131deb.css"
+  },
+  {
+    "revision": "19cb75c3c40340e7817759560099871c",
+    "url": "/coach/dojos.json"
+  },
+  {
+    "revision": "f148d40d6d7ef984f30fe5858817f8fd",
+    "url": "/coach/index.html"
+  },
+  {
+    "revision": "6cbc9e8d8c52b0e480a4",
+    "url": "/coach/js/app.74c22acf.js"
+  },
+  {
+    "revision": "ea77fc657fcb648f174b",
+    "url": "/coach/js/chunk-a4a4dcf4.7121f43e.js"
+  },
+  {
+    "revision": "5df2eb6db305732b1280",
+    "url": "/coach/js/chunk-vendors.b7a5eadf.js"
+  },
+  {
+    "revision": "cdd1c97ee179d204e6d8f6f9a60025d3",
+    "url": "/coach/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/coach/robots.txt"
+  }
+]);
